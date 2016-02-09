8b2a9bd50aac4f9c1fc44adcbe437ae58566e91d
lammerlord92 <lammerlord92@gmail.com>
2016-02-09 18:15:10 +0100

    Atributos puestos para Story (no relaciones)

--- db/migrate/20160209161217_create_stories.rb
+++ db/migrate/20160209161217_create_stories.rb
@@ -1,6 +1,14 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
+      t.string :frontpage
+      t.string :title
+      t.string :description
+      t.string :language
+      t.float :price
+      t.date :release_date
+      t.boolean :published
+      t.integer :num_purchased
 
       t.timestamps null: false
     end
