9ab3475b6bd64a2487c5289fbea7507b23e23977
lammerlord92 <lammerlord92@gmail.com>
2016-02-16 14:16:59 +0100

    Funciona el crear de categorias y el show

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,10 @@ class Category < ActiveRecord::Base
   has_many :story_categories
   has_many :stories, through: :story_categories
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :icon, presence: true
+
   has_attached_file :icon, styles: {medium:"800x600", thumb:"120x100", icon: "50x50"}
   validates_attachment_content_type :icon, content_type: /\Aimage\/.*\Z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  #categories
+  resources :categories
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
