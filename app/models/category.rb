079a00906db170c666fb7a3a17b4d819f97094e6
lammerlord92 <lammerlord92@gmail.com>
2016-02-16 12:58:20 +0100

    Creado formulario base de categorias

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
   has_many :story_categories
   has_many :stories, through: :story_categories
 
-  has_attached_file :icon, styles: {medium:"100x100"}
+  has_attached_file :icon, styles: {medium:"800x600", thumb:"120x100", icon: "50x50"}
   validates_attachment_content_type :icon, content_type: /\Aimage\/.*\Z/
 end
