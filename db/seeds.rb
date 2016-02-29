df4f5b2a4e4df2dcc2c45e1dafa2054cacb3f844
David <davidrubiolucas@gmail.com>
2016-02-29 18:55:01 +0100

    Se introducen historias en seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Cura.create(name: 'Emmanuel', city: cities.first, rol: 'cazador')
 
+
+############################################################################
+#   Creación de categorías. TABLA: categories
+############################################################################
 category1=Category.create!({name: "Drogas", description: "Historia sobre drogas",
                             icon: File.new("public/categories/original/1/seta-icono-new-super-mario-bros-wii-articulo-videojuegos-zehngames.png")})
 category2=Category.create!({name: "Relleno", description: "Esta historia tiene tanto relleno que se convirtió en Hokage",
@@ -26,7 +30,9 @@ category10=Category.create!({name: "Política", description: "Guía de latrocini
                              icon: File.new("public/categories/original/9/tumblr_n2l8l5aVaN1rvner1o1_500.jpg")})
 category5=Category.create!({name: "Vergonzante", description: "Es un espectaculo vergonzante - Shogun 2",
                             icon: File.new("public/categories/original/10/puedo-eliminar-icono-celular-facebook_1_1720410.jpg")})
-
+############################################################################
+#   Creación de usuarios. TABLA: users
+############################################################################
 
 user1 = User.create!({email: 'user1@mail.com', password: '123456789', password_confirmation: '123456789',
          username: 'user1', name: 'user1', surname1: 'surname11', surname2: 'suername12', phone: 619663023,
@@ -68,16 +74,24 @@ user10 = User.create!({email: 'user10@mail.com', password: '123456789', password
                       username: 'user10', name: 'user10', surname1: 'surname101', surname2: 'suername102', phone: 619663023,
                       birthday: '29/02/2004', sku: '159856'})
 
-
+############################################################################
+#   Creación de rols. TABLA: free_users y premium_users
+############################################################################
 userAcc1 = FreeUser.create()
 userAcc2 = PremiumUser.create()
-userAcc3 = FreeUser.create()
 
 
+############################################################################
+#   Actualización del rol de usuario. TABLA: users
+############################################################################
 user4.update_attribute(:role, userAcc2)
 user2.update_attribute(:role, userAcc2)
 user9.update_attribute(:role, userAcc2)
 
+
+############################################################################
+#   Peticiones de amistad. TABLA: request_friendships
+############################################################################
 friendShip1 = RequestFriendship.create!({message: "RequestFriendship16", sender_id: user1.id, recipient_id: user6.id, status: "ACCEPTED"})
 friendShip2 = RequestFriendship.create!({message: "RequestFriendship71", sender_id: user7.id, recipient_id: user1.id, status: "ACCEPTED"})
 friendShip3 = RequestFriendship.create!({message: "RequestFriendship62", sender_id: user6.id, recipient_id: user2.id, status: "ACCEPTED"})
@@ -89,6 +103,9 @@ friendShip8 = RequestFriendship.create!({message: "RequestFriendship410", sender
 friendShip9 = RequestFriendship.create!({message: "RequestFriendship59", sender_id: user5.id, recipient_id: user9.id, status: "ACCEPTED"})
 friendShip10 = RequestFriendship.create!({message: "RequestFriendship105", sender_id: user10.id, recipient_id: user5.id, status: "ACCEPTED"})
 
+############################################################################
+#   Creación de amigos. TABLA: friendships
+############################################################################
 Friendship.createFriendship(friendShip1)
 Friendship.createFriendship(friendShip2)
 Friendship.createFriendship(friendShip3)
@@ -100,24 +117,35 @@ Friendship.createFriendship(friendShip8)
 Friendship.createFriendship(friendShip9)
 Friendship.createFriendship(friendShip10)
 
-@ret = ''
+###############################################
 # Metodo de active_record. Recorre un maximo de 1000 usuarios por defecto
 # documentacion: http://guides.rubyonrails.org/active_record_querying.html
+##################################################
+@ret = ''
 User.find_each do |usuario|
   usuario.friends.each do |amigo|
     @ret << '-' << usuario.username << ' es amigo de ' << amigo.username << '\n'
   end
 end
 
-
 puts @ret
 
-
+############################################################################
+#   Creación de perfiles de usuario. TABLA: profiles
+############################################################################
 perfil1 = Profile.create({name: 'Hola bebés', avatar: 'http://www.image.es', description: 'This is my profile', signature: 'My sign Bebés', user_id: user1.id, profile_status: true})
 perfil2 = Profile.create({name: 'Hola bebés2', avatar: 'http://www.image1.es', description: 'This is my profile1', signature: 'My sign Bebés1', user_id: user2.id, profile_status: true})
 perfil3 = Profile.create({name: 'Hola bebés3', avatar: 'http://www.image2.es', description: 'This is my profile2', signature: 'My sign Bebés2', user_id: user3.id, profile_status: true})
+perfil4 = Profile.create({name: 'Juan', avatar: 'http://www.image.es', description: 'Perfil de Juan', signature: 'Mi firma mola', user_id: user1.id, profile_status: true})
+perfil5 = Profile.create({name: 'Eustaquio', avatar: 'http://www.image1.es', description: 'Perfil de prueba', signature: 'My sign Bebés1', user_id: user2.id, profile_status: true})
+perfil6 = Profile.create({name: 'Rodolfo', avatar: 'http://www.image2.es', description: 'This is my profile2', signature: 'My sign Bebés2', user_id: user3.id, profile_status: true})
 
 
+############################################################################
+#   Creación de historias. TABLA: stories
+################################################################
 
-
-
+story1 = Story.create({title: 'Caperucita', description: 'El cuento de caperucita', language: 'es', price: 10.00, release_date: Time.current.to_s, published: true, num_purchased: 232, profile_id: perfil4.id})
+story2 = Story.create({title: 'Caperucita2', description: 'El cuento de caperucita 2', language: 'es', price: 13.00, release_date: Time.current.to_s, published: true, num_purchased: 231, profile_id: perfil3.id})
+story3 = Story.create({title: 'Don Quijote', description: 'El cuento de Don quijote', language: 'es', price: 15.00, release_date: Time.current.to_s, published: true, num_purchased: 233, profile_id: perfil2.id})
+story4 = Story.create({title: 'Rey León', description: 'El cuento del Rey Leon', language: 'es', price: 19.00, release_date: Time.current.to_s, published: true, num_purchased: 234, profile_id: perfil2.id})
