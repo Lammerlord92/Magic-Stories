f46f5e9de627c0d95b4bb8f176c317f31f182035
David <davidrubiolucas@gmail.com>
2016-03-01 02:00:02 +0100

    Se introducen vistas dinamicas para el control de errores

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module MagicStories
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +23,6 @@ module MagicStories
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.exceptions_app = self.routes
   end
 end
