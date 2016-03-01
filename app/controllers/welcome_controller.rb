214635017654830dfefb6d840310d81b55508889
Juan Manuel Triguero Piñero <juantriguero@live.com>
2016-03-01 01:41:28 +0100

    Borrados index alternativos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:ingeniero] == 'true'
-      render 'rc'
-    else
-      render 'index'
-    end
-  end
-
-  def index2
-    render :layout => nil
-  end
-
-  def popup
+    render 'index'
   end
 end
