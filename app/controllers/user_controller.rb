1065432613b92fbabaaad9bab377962a0a5be8f7
Curro <fragilsan@alum.us.es>
2016-03-02 17:35:49 +0000

    Corrigiendo busqueda de usuarios

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,12 +2,25 @@ class UserController < ApplicationController
 
   def index
 
+   # @q = params[:q]
+    #query = 'username => ? OR name => ? OR surname1 => ? OR surname2 => ? OR email => ?'
+    #if @q
+     # @users = User.where(query, @q)
+    #else
+     # @users = User.all
+    #end
+
     @q = params[:q]
-    query = 'title => ? OR description => ? OR price => ? OR language => ?'
+    query = 'username like ? OR name like ? OR surname1 like ? OR surname2 like ? OR email like ? '
     if @q
-      @users = User.where(query, @q)
+      @users = Story.where(query, "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%", "%#{@q}%")
+      if @users.blank?
+        flash.alert = "User Not found"
+      end
     else
       @users = User.all
     end
+
+
   end
 end
