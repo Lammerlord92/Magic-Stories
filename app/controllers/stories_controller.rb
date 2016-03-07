8a1c5853d2d9af3fc292bef261de75a2f89f50a2
juandjara <juanorigami@gmail.com>
2016-03-07 15:20:39 +0100

    fixed histories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,9 @@
 class StoriesController < ApplicationController
-  before_action :params_search, only: [:search]
 
   #GET /stories
+  def index
+    @stories = Story.all
+  end
 
   #GET /stories/read/:id
   def read
@@ -39,54 +41,35 @@ class StoriesController < ApplicationController
     end
   end
 
-
   def story_params
     # Con frontpage -> params.require(:story).permit(:title,:description,:cover,:frontpage,:language,:price,:release_date,:published,:num_purchased)
     params.require(:story).permit(:title,:description,:cover,:language,:price,:release_date,:published,:num_purchased)
   end
 
-  #TODO Aun no se pueden adquirir historias
+  #TODO ¿ Aun no se pueden adquirir historias ?
   def show_stories_acquired
-    @stories = []
+    @stories = Story.find_by_profile_id(params[:id])
   end
 
-
-    def show_stories_acquired
-      @stories = Story.find_by_profile_id(params[:id])
-    end
-
-
-    def read
-      # @story = Story.find(params[:id])
-    end
-
-
   def show_stories_created
-    profile = Profile.find_by(user: current_user)
+    profile  = Profile.find_by(user: current_user)
     @stories = Story.where(creatorProfile: profile)
   end
 
-  def params_search
-    params.permit(:q)
-  end
-
-
-
-def search
-  # Documentacion sobre queries aqui:
-  # http://guides.rubyonrails.org/active_record_querying.html
-  @q = params[:q].downcase
-  query = 'lower(title) like :q OR lower(description) like :q OR lower(language) like :q and published = true'
-  if @q
-    @stories = Story.where(query, {q: "%#{@q}%"})
-    if @stories.blank?
-      flash.alert = "Not found"
+  def search
+    # Documentacion sobre queries aqui:
+    # http://guides.rubyonrails.org/active_record_querying.html
+    @q = params[:q].downcase
+    query = 'lower(title) like :q OR lower(description) like :q OR lower(language) like :q and published = true'
+    if @q
+      @stories = Story.where(query, {q: "%#{@q}%"})
+      if @stories.blank?
+        flash.alert = 'Not found'
+      end
+    else
+      @stories = Story.where(published: true)
     end
-  else
-    @stories = Story.where(published: true)
+    render 'index'
   end
-
-  render 'list'
-end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20160223191838) do
     t.integer  "story_id"
   end
 
+  add_index "chapters", ["story_id"], name: "index_chapters_on_story_id", using: :btree
+
   create_table "discount_user_groups", force: :cascade do |t|
     t.integer  "user_group_id"
     t.integer  "discount_id"
@@ -163,7 +165,14 @@ ActiveRecord::Schema.define(version: 20160223191838) do
     t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.string   "profile_status"
+    t.boolean  "profile_status"
+  end
+
+  create_table "readings", force: :cascade do |t|
+    t.integer  "story_id"
+    t.integer  "profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reports", force: :cascade do |t|
@@ -251,6 +260,7 @@ ActiveRecord::Schema.define(version: 20160223191838) do
   add_foreign_key "additions", "discounts"
   add_foreign_key "additions", "profiles"
   add_foreign_key "additions", "stories"
+  add_foreign_key "chapters", "stories"
   add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", column: "conversation_id", name: "mb_opt_outs_on_conversations_id"
   add_foreign_key "mailboxer_notifications", "mailboxer_conversations", column: "conversation_id", name: "notifications_on_conversation_id"
   add_foreign_key "mailboxer_receipts", "mailboxer_notifications", column: "notification_id", name: "receipts_on_notification_id"
