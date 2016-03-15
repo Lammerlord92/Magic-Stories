40e2c11b5c0ae1243f7976ad2bc71b395437b35b
Jukar <mindundi@debian>
2016-03-15 14:57:34 +0100

    Creada la vista maker5 con el editor de texto

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   get 'welcome/maker2'
   get 'welcome/maker3'
   get 'welcome/maker4'
+  get 'welcome/maker5'
   get 'welcome/textoDesplegable'
 
   # You can have the root of your site routed with "root"
--- db/schema.rb
+++ db/schema.rb
@@ -85,19 +85,6 @@ ActiveRecord::Schema.define(version: 20160311192414) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
-    t.datetime "created_at"
-  end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
@@ -183,13 +170,6 @@ ActiveRecord::Schema.define(version: 20160311192414) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "profile_status"
-  end
-
-  create_table "readings", force: :cascade do |t|
-    t.integer  "story_id"
-    t.integer  "profile_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "reports", force: :cascade do |t|
