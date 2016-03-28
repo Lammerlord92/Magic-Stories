5b9049e1d4496249237b8af206d6158b954d6e23
Juan Francisco Nieto Mendoza <juaniemen@hotmail.com>
2016-03-28 14:35:20 +0200

    Cambios en ApplicationHelper. Métodos de autorización. Accesibles desde toda la aplicación.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,27 +4,56 @@ module ApplicationHelper
 Métodos para controlar el acceso a las distintas operaciones.
 Estos métodos son accesibles desde cualquier controlador.
 Los métodos con interrogación devuelven true-false
-Los métodos con exclamación lanzan una except.
+Los métodos con exclamación lanzan un RuntimeError.
 
 =end
   def checkPrincipal?(user)
-    current_user.id.equal?(user.id)
+    current_user.id.eql?(user.id)
   end
 
-  def checkRole?(user)
-    current_user.role.equal?(user.role)
+  def checkPrincipal!(user)
+    if(!current_user.id.eql?(user.id))
+      raise 'Operacion no permitida.'
+    end
   end
 
-  def checkRole!(user)
-    if(current_user.id.equal?(user.id))
-      raise 'Operacion no permitida'
+  ## Recibe un usuario y un rol, comprueba que ese usuario tiene ese rol.
+  ## Hay que pasarle la clase del rol, sería algo así checkRole?("Administrator", usuario1)
+  def checkRole?(role, user)
+    user.role_type.eql?(role)
+  end
+
+  def checkRole!(role, user)
+    if(!user.role_type.eql?(role))
+      raise 'Operacion no permitida. Su cuenta no dispone de privilegios suficientes'
     end
   end
 
 
-  def checkRole!(user)
-    if(current_user.role.equal?(user.role))
-      raise "Operacion no permitida"
-    end
+
+  ## Métodos más especificos por usuarios actuales
+  def checkAdmin?(user)
+    checkRole?("Administrator", user)
   end
+
+  def checkAdmin!(user)
+    checkRole!("Administrator", user)
+  end
+
+  def checkFreeUser?(user)
+    checkRole?("FreeUser", user)
+  end
+
+  def checkFreeUser!(user)
+    checkRole!("FreeUser", user)
+  end
+
+  def checkPremiumUser?(user)
+    checkRole?("PremiumUser", user)
+  end
+
+  def checkPremiumUser!(user)
+    checkRole?("PremiumUser", user)
+  end
+
 end
