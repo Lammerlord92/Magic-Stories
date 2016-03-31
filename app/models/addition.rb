a43541919d8b73bed9f14ca8ac839e43e8e37651
Lammerlord92 <javcarfer92@gmail.com>
2016-03-31 16:05:51 +0200

    Añadida addition(solo el modelo) a Cassandra

--- app/models/addition.rb
+++ app/models/addition.rb
@@ -1,5 +1,16 @@
-class Addition < ActiveRecord::Base
-  belongs_to :discount
-  belongs_to :profile
-  belongs_to :story
+class Addition
+  include Cequel::Record
+
+  key :id, :timeuuid, auto: true
+  column :purchase_date, :timestamp
+  column :base_price, :double
+#  belongs_to :discount
+#  belongs_to :profile
+#  belongs_to :story
+
+  #Al crear la addition, mete la fecha actual
+  before_create :change_time
+  def change_time
+    self.purchase_date = Time.now
+  end
 end
