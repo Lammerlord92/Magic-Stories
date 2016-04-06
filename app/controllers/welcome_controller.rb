049663ebeb78b3f186d753c9e1ffdb68f9af9d09
Juan Francisco Nieto Mendoza <juaniemen@hotmail.com>
2016-04-06 19:45:04 +0200

    Redirección unsigned user.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,26 +1,28 @@
 class WelcomeController < ApplicationController
-  before_filter :require_login
   def index
 =begin
      HAY QUE CONTROLAR QUE SI NO ESTA ACTIVA UNA SESIÓN SE MUESTRE EXCLUSIVAMENTE EL LOGIN
           QUE ES LA VISTA DEL sessions/new.html.erb DEL DEVISE
 
-    if !user_signed_in?
-      redirect_to "new_user_session_path"
-    else
+
 =end
 
-
-      @story = Story.first # Temporalmente para pruebas del index
-      @categories = Category.all
-      render 'index'
-  end
-  private
-
-  def require_login
-    unless current_user
-      redirect_to "new_user_session_path"
-    end
+  if !user_signed_in?
+    redirect_to "/user/sign_in"
+  else
+    @story = Story.first # Temporalmente para pruebas del index
+    @categories = Category.all
+    render 'index'
   end
 
+  # private
+
+  # def require_login
+  #   unless current_user
+  #     redirect_to "/user/sign_in
+  #   end
+  # end
+
+  end
 end
+
