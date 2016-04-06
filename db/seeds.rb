4138afb48ebeb7a7ca8dc73f20fd91c35af8ae30
juandjara <juanorigami@gmail.com>
2016-04-06 18:26:10 +0200

    cambios de triguero grammarnaziying

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,34 +35,34 @@ puts "Success: Todas las tablas han sido borradas"
 ############################################################################
 
 category1 = Category.create!({
-  name: 'Drama',
-  description: 'Género dramático'
-})
+                                 name: 'Drama',
+                                 description: 'Género dramático'
+                             })
 
 category2 = Category.create!({
-  name: 'Cuento',
-  description: 'Género de cuentos'
-})
+                                 name: 'Cuento',
+                                 description: 'Género de cuentos'
+                             })
 
 category3 = Category.create!({
-  name: 'Ficción',
-  description: 'Género de ficción'
-})
+                                 name: 'Ficción',
+                                 description: 'Género de ficción'
+                             })
 
 category4 = Category.create!({
-  name: 'Terror',
-  description: 'Género de terror'
-})
+                                 name: 'Terror',
+                                 description: 'Género de terror'
+                             })
 
 category5 = Category.create!({
-  name: 'Épico',
-  description: 'Género épico'
-})
+                                 name: 'Épico',
+                                 description: 'Género épico'
+                             })
 
 category6 = Category.create!({
-  name: 'Aventura',
-  description: 'Género de aventuras'
-})
+                                 name: 'Aventura',
+                                 description: 'Género de aventuras'
+                             })
 
 puts "Success: Creación de categorías"
 
@@ -72,8 +72,8 @@ puts "Success: Creación de categorías"
 
 
 user1 = User.create!({email: 'user1@mail.com', password: '123456789', password_confirmation: '123456789',
-         username: 'user1', name: 'user1', surname1: 'surname11', surname2: 'suername12', phone: 619663023,
-         birthday: '30/10/1993', sku: '132563'})
+                      username: 'user1', name: 'user1', surname1: 'surname11', surname2: 'suername12', phone: 619663023,
+                      birthday: '30/10/1993', sku: '132563'})
 
 user2 = User.create!({email: 'user2@mail.com', password: '123456789', password_confirmation: '123456789',
                       username: 'user2', name: 'user2', surname1: 'surname21', surname2: 'suername22', phone: 619663023,
@@ -108,8 +108,8 @@ user9 = User.create!({email: 'user9@mail.com', password: '123456789', password_c
                       birthday: '17/02/1993', sku: '352698'})
 
 user10 = User.create!({email: 'user10@mail.com', password: '123456789', password_confirmation: '123456789',
-                      username: 'user10', name: 'user10', surname1: 'surname101', surname2: 'suername102', phone: 619663023,
-                      birthday: '29/02/2004', sku: '159856'})
+                       username: 'user10', name: 'user10', surname1: 'surname101', surname2: 'suername102', phone: 619663023,
+                       birthday: '29/02/2004', sku: '159856'})
 user11 = User.create!({email: 'winslopu@hotmail.com', password: 'Pegaso33', password_confirmation: 'Pegaso33',
                        username: 'Winslou', name: 'David', surname1: 'Rubio', surname2: 'Lucas', phone: 645544778,
                        birthday: '29/02/2004', sku: '159857'})
@@ -233,7 +233,7 @@ puts "Success: Creación de ActorUserGroup"
 # Creacion de Story  -- Unión a profile
 
 story1 = Story.create!({title: 'Title1', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '20/10/2015',
-              published: true, num_purchased: 0, creatorProfile: user1.profile})
+                        published: true, num_purchased: 0, creatorProfile: user1.profile})
 
 story2 = Story.create!({title: 'Title2', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '20/10/2015',
                         published: true, num_purchased: 0, creatorProfile: user1.profile})
@@ -260,13 +260,13 @@ story9 = Story.create!({title: 'Title9', description: 'Description', language: '
                         published: true, num_purchased: 0, creatorProfile: user3.profile})
 
 story10 = Story.create!({title: 'Title10', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '21/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user3.profile})
+                         published: true, num_purchased: 0, creatorProfile: user3.profile})
 
 story11 = Story.create!({title: 'Title11', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '21/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user3.profile})
+                         published: true, num_purchased: 0, creatorProfile: user3.profile})
 
 story12 = Story.create!({title: 'Title12', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '21/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user3.profile})
+                         published: true, num_purchased: 0, creatorProfile: user3.profile})
 puts "Success: Creación de historias"
 
 user1.profile.stories << story1
@@ -398,13 +398,13 @@ end
 node1 = <<-EOF
 
 <p>
-Habia una vez una ninya muy bonita. Su madre le habia hecho una capa roja y la muchachita la llevaba tan a menudo que todo el mundo la llamaba caperucita roja.
+Había una vez una niña muy bonita. Su madre le había hecho una capa roja y la muchachita la llevaba tan a menudo que todo el mundo la llamaba caperucita roja.
 </p>
 <p>
-Un dia, su madre le pidio que llevase unos pasteles a su abuela que vivia en el otro lado del bosque, recomendandole que no se entretuviese por el camino, pues cruzar el bosque era muy peligroso, ya que siempre andaba acechando por alli el lobo
+Un día, su madre le pidió que llevase unos pasteles a su abuela que vivía en el otro lado del bosque, recomendándole que no se entretuviese por el camino, pues cruzar el bosque era muy peligroso, ya que siempre andaba acechando por alli el lobo.
 </p>
 <p>
-Caperucita Roja recogio la cesta con los pasteles y se puso en camino. La ninya tenia que atravesar el bosque para llegar a casa de la Abuelita, pero no le daba miedo porque alli siempre se encontraba con muchos amigos: los pajaros, las ardillas ...
+Caperucita Roja recogió la cesta con los pasteles y se puso en camino. La niña tenía que atravesar el bosque para llegar a casa de la Abuelita, pero no le daba miedo, porque allí siempre se encontraba con muchos amigos: los pájaros, las ardillas...
 </p>
 <p>
 De repente, vio al lobo, enorme, delante de ella.
@@ -414,20 +414,20 @@ EOF
 
 node2 = <<-EOF
 
-<p> -
-¿A dónde vas, niña? - le preguntó el lobo con su voz ronca
+<p>
+- ¿A dónde vas, niña? - le preguntó el lobo con su voz ronca.
 </p>
 <p>
-- A casa de mi Abuelita - le dijo Caperucita
+- A casa de mi abuelita - le dijo Caperucita.
 </p>
 <p>
-- No está lejos - pensó el lobo para sí, dándose media vuelta.
+"No está lejos", pensó el lobo para sí, dándose media vuelta.
 </p>
 <p>
-Caperucita puso su cesta en la hierba y se entretuvo cogiendo flores: - El lobo se ha ido - pensó-, no tengo nada que temer. La abuela se pondrá muy contenta cuando le lleve un hermoso ramo de flores además de los pasteles
+Caperucita puso su cesta en la hierba y se entretuvo cogiendo flores. "El lobo se ha ido", pensó. "No tengo nada que temer. La abuela se pondrá muy contenta cuando le lleve un hermoso ramo de flores además de los pasteles"
 </p>
 <p>
-Mientras tanto, el lobo se fue a casa de la Abuelita, llamó suavemente a la puerta y la anciana le abrió pensando que era Caperucita. Un cazador que pasaba por allí había observado la llegada del lobo
+Mientras tanto, el lobo se fue a casa de la Abuelita y llamó suavemente a la puerta; la anciana le abrió pensando que era Caperucita. Un cazador que pasaba por allí había observado la llegada del lobo.
 </p>
 <p>
 El lobo devoró a la Abuelita y se puso el gorro rosa de la desdichada, se metió en la cama y cerró los ojos. No tuvo que esperar mucho, pues Caperucita Roja llegó enseguida, toda contenta.
@@ -439,22 +439,22 @@ La niña se acercó a la cama y vio que su abuela estaba muy cambiada.
 - Abuelita, abuelita, ¡qué ojos más grandes tienes!
 </p>
 <p>
-- Son para verte mejor- dijo el lobo tratando de imitar la voz de la abuela.
+- Son para verte mejor - dijo el lobo tratando de imitar la voz de la abuela.
 </p>
 <p>
 - Abuelita, abuelita, ¡qué orejas más grandes tienes!
 </p>
 <p>
-- Son para oírte mejor- siguió diciendo el lobo.
+- Son para oírte mejor - siguió diciendo el lobo.
 </p>
 <p>
 - Abuelita, abuelita, ¡qué dientes más grandes tienes!
 </p>
 <p>
-- Son para...¡comerte mejoooor!- y diciendo esto, el lobo malvado se abalanzó sobre la niñita y la devoró, lo mismo que había hecho con la abuelita.
+- Son para... ¡comerte mejoooor! - y, diciendo esto, el lobo malvado se abalanzó sobre la niñita y la devoró, lo mismo que había hecho con la Abuelita.
 </p>
 <p>
-Mientras tanto, el cazador se había quedado preocupado y creyendo adivinar las malas intenciones del lobo, decidió echar un vistazo a ver si todo iba bien en la casa de la Abuelita. Pidió ayuda a un segador y los dos juntos llegaron al lugar. Vieron la puerta de la casa abierta y al lobo tumbado en la cama, dormido de tan harto que estaba.
+Mientras tanto, el cazador se había quedado preocupado y, creyendo adivinar las malas intenciones del lobo, decidió echar un vistazo a ver si todo iba bien en la casa de la Abuelita. Pidió ayuda a un segador y los dos juntos llegaron al lugar. Vieron la puerta de la casa abierta y al lobo tumbado en la cama, dormido de tan harto que estaba.
 </p>
 <p>
 El cazador sacó su cuchillo y rajó el vientre del lobo. La Abuelita y Caperucita estaban allí, ¡vivas!.
@@ -471,31 +471,31 @@ EOF
 node3 = <<-EOF
 
 <p>
-- ¿A dónde vas, niña?- le preguntó el lobo con su voz ronca.
+- ¿A dónde vas, niña? - le preguntó el lobo con su voz ronca.
 </p>
 <p>
-- A ti que te importa- le espetó Caperucita.
+- ¿Y a ti qué te importa? - le espetó Caperucita.
 </p>
 <p>
-- Un poco borde para ser tan pequeña- le dijo el lobo, mientras se acercaba lentamente.
+- Un poco borde para ser tan pequeña, ¿no? - le dijo el lobo, mientras se acercaba lentamente.
 </p>
 <p>
-Caperucita intentaba contener sus nervios, pero era imposible evitar temblar. El lobo le rozaba con la cola lentamente, mientras la rodeaba.
+Caperucita intentaba contener sus nervios, pero le era imposible evitar temblar. El lobo le rozaba con la cola lentamente, mientras la rodeaba.
 </p>
 <p>
--No me da miedo un lobo como tú. –gritó caperucita esperando que alguien la oyera.
+- ¡No me da miedo un lobo como tú! – gritó Caperucita, esperando que alguien la oyera.
 </p>
 <p>
--¿Cómo yo?, –El lobo soltó una leve risa.-no existen lobos como yo.
+-¿Como yo? – el lobo soltó una leve risa -. No existen lobos como yo.
 </p>
 <p>
--Déjame en paz. –Caperucita mientras decía esto le lanzo la cesta a la cabeza, y salió huyendo de vuelta a su casa.
+- Déjame en paz – mientras decía esto, Caperucita le lanzó la cesta a la cabeza, y salió huyendo de vuelta a su casa.
 </p>
 <p>
-El lobo se abalanzó sobre ella mordiéndola en el cuello, la sangre caía a borbotones, el lobo empezó su propio festín.
+El lobo se abalanzó sobre ella, mordiéndola en el cuello. La sangre caía a borbotones. El lobo empezó su propio festín.
 </p>
 <p>
-Caperucita nunca llegó a casa de su abuela y el lobo volvió al bosque donde nunca se supo más de él.
+Caperucita nunca llegó a casa de su abuela y el lobo volvió al bosque, y nunca se supo más de él.
 </p>
 <p>
 Días más tarde la madre denunció su desaparición y un cazador encontró el cadáver desfigurado de la niña. Fue un día triste para todos.
@@ -506,16 +506,15 @@ EOF
 node4 = <<-EOF
 
 <p>
-- ¿A dónde vas, niña?- le preguntó el lobo con su voz ronca.
+- ¿A dónde vas, niña? - le preguntó el lobo con su voz ronca.
 </p>
 <p>
-- A casa de mi Abuelita, a no ser que me salga un plan mejor- le dijo pícaramente caperucita.
+- A casa de mi Abuelita, a no ser que me salga un plan mejor - le dijo pícaramente caperucita.
 </p>
 <p>
--Muy pequeña para esa mirada - El lobo se acercó lentamente - ¿Cuántos años tienes?
+- Eres muy pequeña para tener esa mirada - el lobo se acercó lentamente -. ¿Cuántos años tienes?
 </p>
 <p>
-
 - Lo dices como si con este cuerpo me fueran a preguntar en la discoteca.
 </p>
 <p>
@@ -525,25 +524,25 @@ node4 = <<-EOF
 - ¿Por qué no?
 </p>
 <p>
-El lobo se quedó pensativo, mientras observaba el cuerpo de caperucita parecía mayor para su edad, pero claro no sabía su edad, le daba miedo que fuera menor.
+El lobo se quedó pensativo, mientras observaba el cuerpo de Caperucita. Parecía mayor para su edad, pero claro, tampoco sabía qué edad tenía. Al lobo le daba miedo que fuera menor.
 </p>
 <p>
-- ¿nos vamos o qué? –caperucita estaba mascando un chicle y le daba vueltas a la cesta impaciente.
+- ¿Nos vamos o qué? – Caperucita estaba mascando un chicle y le daba vueltas a la cesta, impaciente.
 </p>
 <p>
 - Espero que no me des problemas.
 </p>
 <p>
-    Ambos se fueron en una moto, cuando salieron del bosque llegaron a una discoteca, donde pasaron parte de la tarde. Caperucita bebió bastante alcohol. Más adelante la abuela de caperucita se presentó en la discoteca, ataviada con su bata de estar por casa, las sandalias y el pelo recogido con unos rulos.
+Ambos se fueron en la moto del lobo. Cuando salieron del bosque llegaron a una discoteca, donde pasaron parte de la tarde. Caperucita bebió bastante alcohol. Más adelante, la abuela de caperucita se presentó en la discoteca, ataviada con su bata de estar por casa, las sandalias y el pelo recogido con unos rulos.
 </p>
 <p>
-La abuela sacó su cuchillo y rajó el vientre del lobo. La Abuelita se llevó a Caperucita de las orejas.
+La abuela sacó su cuchillo y rajó el vientre del lobo. Luego se llevó a Caperucita de las orejas.
 </p>
 <p>
-    Para castigar al lobo malo, el cazador que pasaba por allí le llenó el vientre de piedras y luego lo volvió a cerrar. Cuando el lobo despertó de su pesado sueño, sintió muchísima sed y se dirigió a un estanque próximo para beber. Como las piedras pesaban mucho, cayó en el estanque de cabeza y se ahogó.
+Para castigar al lobo malo, el cazador que pasaba por allí le llenó el vientre de piedras y luego lo volvió a cerrar. Cuando el lobo despertó de su pesado sueño, sintió muchísima sed y se dirigió a un estanque próximo para beber. Como las piedras pesaban mucho, cayó en el estanque de cabeza y se ahogó.
 </p>
 <p>
-    En cuanto a Caperucita estuvo castigada sin salir varios meses.
+En cuanto a Caperucita, estuvo castigada sin salir varios meses.
 
 </p>
 
