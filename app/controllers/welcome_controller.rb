e5892dec0370d9373ef80c33a78fe23dcf29b923
Antonio D. G. Medina <antgarmed@outlook.com>
2016-04-07 13:35:44 +0200

    Index terminado (solo falta los iconos de los géneros)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,6 +10,15 @@ class WelcomeController < ApplicationController
     @trending_stories[Category.find(5)] = Story.find(5)
     @trending_stories[Category.find(6)] = Story.find(6)
 
+    # TODO: Provisional. El backend tiene que insertar listas de historias tendencias de cada género
+    @first_six_trending_stories = Hash.new
+    @first_six_trending_stories[Category.find(1)] = [Story.find(1), Story.find(2), Story.find(7), Story.find(8), Story.find(9), Story.find(10)]
+    @first_six_trending_stories[Category.find(2)] = [Story.find(13), Story.find(3), Story.find(4), Story.find(5), Story.find(6), Story.find(12)]
+    @first_six_trending_stories[Category.find(3)] = [Story.find(3), Story.find(8), Story.find(9), Story.find(10), Story.find(11), Story.find(4)]
+    @first_six_trending_stories[Category.find(4)] = [Story.find(4), Story.find(5), Story.find(6), Story.find(7), Story.find(8), Story.find(9)]
+    @first_six_trending_stories[Category.find(5)] = [Story.find(5), Story.find(6), Story.find(7), Story.find(8), Story.find(9), Story.find(10)]
+    @first_six_trending_stories[Category.find(6)] = [Story.find(6), Story.find(5), Story.find(6), Story.find(7), Story.find(8), Story.find(9)]
+
     @categories = Category.all
     render 'index'
   end
