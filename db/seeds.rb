2e9259d5c52c121f290f39d33b35f6c95d8242c8
Juan Manuel Triguero Piñero <juantriguero@live.com>
2016-04-07 16:40:03 +0200

    Parche portadas de tendencias

--- db/seeds.rb
+++ db/seeds.rb
@@ -233,22 +233,22 @@ puts "Success: Creación de ActorUserGroup"
 # Creacion de Story  -- Unión a profile
 
 story1 = Story.create!({title: 'Title1', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '20/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user1.profile})
+                        published: true, num_purchased: 0, creatorProfile: user1.profile, cover: 'http://www.upl.co/uploads/drama1460039396.jpg'})
 
 story2 = Story.create!({title: 'Title2', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '20/10/2015',
                         published: true, num_purchased: 0, creatorProfile: user1.profile})
 
 story3 = Story.create!({title: 'Title3', description: 'Description', language: 'ENGLISH', price: 16.30, release_date: '20/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user1.profile})
+                        published: true, num_purchased: 0, creatorProfile: user1.profile, cover: 'http://www.upl.co/uploads/ficcion1460039396.jpg'})
 
 story4 = Story.create!({title: 'Title4', description: 'Description', language: 'ENGLISH', price: 16.30, release_date: '20/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user1.profile})
+                        published: true, num_purchased: 0, creatorProfile: user1.profile, cover: 'http://www.upl.co/uploads/Terror1460039396.jpg'})
 
 story5 = Story.create!({title: 'Title5', description: 'Description', language: 'CHINESE', price: 16.30, release_date: '20/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user2.profile})
+                        published: true, num_purchased: 0, creatorProfile: user2.profile, cover: 'http://www.upl.co/uploads/epico1460039396.jpg'})
 
 story6 = Story.create!({title: 'Title6', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '20/10/2015',
-                        published: true, num_purchased: 0, creatorProfile: user2.profile})
+                        published: true, num_purchased: 0, creatorProfile: user2.profile, cover: 'http://www.upl.co/uploads/aventura1460039396.jpg'})
 
 story7 = Story.create!({title: 'Title7', description: 'Description', language: 'SPANISH', price: 16.30, release_date: '20/10/2015',
                         published: true, num_purchased: 0, creatorProfile: user2.profile})
