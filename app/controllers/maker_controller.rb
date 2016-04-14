1baa3bc85563e436f20ab3a791c7b2481f855145
Lammerlord92 <javcarfer92@gmail.com>
2016-04-14 17:18:20 +0200

    Limpiar maker

--- app/controllers/maker_controller.rb
+++ app/controllers/maker_controller.rb
@@ -1,17 +1,7 @@
 class MakerController < ApplicationController
-  def sample
-    render 'pruebas_maker/sample'
-  end
-
   def show
     @id = params[:id]
-    #@auxMaker = AuxMaker.new
-    #@auxMaker.storyId = @id
     render 'welcome/maker'
-  end
-
-  def save
-    #TODO
   end
 
 end
