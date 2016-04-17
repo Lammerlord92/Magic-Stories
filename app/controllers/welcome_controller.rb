59b7ffed62c9b4bee2e9c24b02a29657990234bc
Antonio D. G. Medina <antgarmed@uSatellite>
2016-04-17 19:13:18 +0200

    WelcomeController adaptado para el nuevo index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,26 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    @categories = Category.all
+
     # TODO: Provisional. El backend tiene que insertar la historia tendencia de cada género
     @trending_stories = Hash.new
-    @trending_stories[Category.find(1)] = Story.find(1)
-    @trending_stories[Category.find(2)] = Story.find(13)
-    @trending_stories[Category.find(3)] = Story.find(3)
-    @trending_stories[Category.find(4)] = Story.find(4)
-    @trending_stories[Category.find(5)] = Story.find(5)
-    @trending_stories[Category.find(6)] = Story.find(6)
+    @trending_stories[@categories[0]] = Story.find(1)
+    @trending_stories[@categories[1]] = Story.find(13)
+    @trending_stories[@categories[2]] = Story.find(3)
+    @trending_stories[@categories[3]] = Story.find(4)
+    @trending_stories[@categories[4]] = Story.find(5)
+    @trending_stories[@categories[5]] = Story.find(6)
 
-    # TODO: Provisional. El backend tiene que insertar listas de historias tendencias de cada género
-    @first_six_trending_stories = Hash.new
-    @first_six_trending_stories[Category.find(1)] = [Story.find(1), Story.find(2), Story.find(7), Story.find(8), Story.find(9), Story.find(10)]
-    @first_six_trending_stories[Category.find(2)] = [Story.find(13), Story.find(3), Story.find(4), Story.find(5), Story.find(6), Story.find(12)]
-    @first_six_trending_stories[Category.find(3)] = [Story.find(3), Story.find(8), Story.find(9), Story.find(10), Story.find(11), Story.find(4)]
-    @first_six_trending_stories[Category.find(4)] = [Story.find(4), Story.find(5), Story.find(6), Story.find(7), Story.find(8), Story.find(9)]
-    @first_six_trending_stories[Category.find(5)] = [Story.find(5), Story.find(6), Story.find(7), Story.find(8), Story.find(9), Story.find(10)]
-    @first_six_trending_stories[Category.find(6)] = [Story.find(6), Story.find(5), Story.find(6), Story.find(7), Story.find(8), Story.find(9)]
+    # TODO: Provisional. El backend tiene que insertar las historias más nuevas de cada género
+    @newest_stories_by_category = Hash.new
+    @newest_stories_by_category[@categories[0]] = [Story.find(1), Story.find(2), Story.find(3)]
+    @newest_stories_by_category[@categories[1]] = [Story.find(13), Story.find(4), Story.find(5)]
+    @newest_stories_by_category[@categories[2]] = [Story.find(6), Story.find(7), Story.find(8)]
+    @newest_stories_by_category[@categories[3]] = [Story.find(9), Story.find(10), Story.find(11)]
+    @newest_stories_by_category[@categories[4]] = [Story.find(12), Story.find(1), Story.find(2)]
+    @newest_stories_by_category[@categories[5]] = [Story.find(3), Story.find(4), Story.find(5)]
 
-    @categories = Category.all
     render 'index'
   end
 end
