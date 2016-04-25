78cbbed1562c08b3ce039da69fd17b99de4800a3
Lammerlord92 <javcarfer92@gmail.com>
2016-04-25 19:04:09 +0200

    Commiteado 1ª versión de los controladores de capítulos y opciones. Opciones no me lo pilla como clase ruby en el editor de momento

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,8 +1,8 @@
 class ChaptersController < ApplicationController
-
+  #protect_from_forgery with: :null_session
   # GET /chapters/1.jsonº
   # GET /chapters/1.json{"id":2,"title":Capítulo 01,"body":Este es el texto del capítulo 01. Este capítulo no puede borrarse, ya que es el primer capítulo de tu historia. Puedes crear nuevos capítulos y enlaces mediante los botones de la izquierda.,"child_options":[{"child_id":3,"parent_id":2,"option":1985"}],"parent_options":[]}
-
+  respond_to :json, only: [:create,:update,:destroy]
   def show
     begin
       @chapter = Chapter.find(params[:id])
@@ -12,12 +12,11 @@ class ChaptersController < ApplicationController
   end
 
   def create
-    @chapter = Chapter.find_or_create_by({id: params[:chapter][:id]})
+    @chapter = Chapter.new(chapter_params)
+    @chapter.save
 
-    @chapter.body = chapter_params[:body]
-    @chapter.title = chapter_params[:title]
-    @chapter.story_id = chapter_params[:story_id]
-
+#    redirect_to_maker_view
+=begin
     respond_to do |format|
       if @chapter.save
         format.json { render action: 'show', status: :ok }
@@ -25,33 +24,52 @@ class ChaptersController < ApplicationController
         format.json { render json: @chapter.errors, status: :unprocessable_entity }
       end
     end
-
+=end
   end
 
-  def create_options
-    id = params[:chapter][:id]
-    @chapter = Chapter.find(id)
+  def update
+    @chapter = chapter_find
     if !@chapter
       render json: {error: "Error: No existe el capitulo con id #{id}"},
              status: :unprocessable_entity
     else
-      parents = chapter_params[:parent_options]
-      if parents
-        parents.map! do |parent|
-          {child_id: id, parent_id: parent}
-        end
+      format.json { render json: @chapter.errors, status: :unprocessable_entity }
+    end
+    @chapter.update(chapter_params)
+#    redirect_to_maker_view
+  end
+  def destroy
+    @chapter=chapter_find
+    @chapter.destroy
+ #   redirect_to_maker_view
+  end
 
-        parents.map! do |parsed_parent|
-          option = Option.find_or_initialize_by(parsed_parent)
-          unless option.option
-            option.option = "Conecta nodo #{parsed_parent[:child_id]} con #{parsed_parent[:parent_id]}"
-          end
-          unless option.save
-            render json: @chapter.errors, status: :unprocessable_entity
-          end
-        end
-      end
+=begin
+ def create_options
+   id = params[:chapter][:id]
+   @chapter = Chapter.find(id)
+   if !@chapter
+     render json: {error: "Error: No existe el capitulo con id #{id}"},
+            status: :unprocessable_entity
+   else
+     parents = chapter_params[:parent_options]
+     if parents
+       parents.map! do |parent|
+         {child_id: id, parent_id: parent}
+       end
 
+       parents.map! do |parsed_parent|
+         option = Option.find_or_initialize_by(parsed_parent)
+         unless option.option
+           option.option = "Conecta nodo #{parsed_parent[:child_id]} con #{parsed_parent[:parent_id]}"
+         end
+         unless option.save
+           render json: @chapter.errors, status: :unprocessable_entity
+         end
+       end
+     end
+=end
+=begin
       children = chapter_params[:child_options]
       if children
         children.each_pair do |index, child|
@@ -67,18 +85,25 @@ class ChaptersController < ApplicationController
 
     end
   end
+=end
 
   private
+    def chapter_find
+      @chapter = Chapter.find(params[:id])
+    end
+
+    def redirect_to_maker_view
+      redirect_to controller: 'maker_controller', action: 'show', id: params[:story_id]
+    end
+
     def chapter_params
       params
           .require(:chapter)
           .permit(
-              {child_options: [:option, :child_id, :parent_id]},
-              :story_id,
+              :id,
               :body,
               :title,
-              :id,
-              parent_options: []
+              :story_id
           )
     end
 
---
+++ app/controllers/option_controller.rb
@@ -0,0 +1,43 @@
+class OptionController < ApplicationController
+    respond_to :json, only: [:create,:update,:destroy]
+
+  def create
+    @option = Option.new(option_params)
+    @option.save
+  end
+
+  def update
+    @option = option_find
+    if !@option
+      render json: {error: "Error: No existe la opción con id #{id}"},
+             status: :unprocessable_entity
+    else
+      format.json { render json: @option.errors, status: :unprocessable_entity }
+    end
+    @option.update(option_params)
+#    redirect_to_maker_view
+  end
+
+  def destroy
+    @option=option_find
+    @option.destroy
+ #   redirect_to_maker_view
+  end
+
+    private
+      def option_find
+        @option = Option.find(params[:id])
+      end
+
+      def option_params
+        params
+            .require(:chapter)
+            .permit(
+                :id,
+                :parent_id,
+                :child_id,
+                :option
+            )
+      end
+
+end
