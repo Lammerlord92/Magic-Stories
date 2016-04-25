2eed3a35f1e06d7afcef435c4d20b93d7c2c17f0
Lammerlord92 <javcarfer92@gmail.com>
2016-04-25 22:30:41 +0200

    Añadido código comentado para hacer una api

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,36 +1,37 @@
-class OptionController < ApplicationController
-  respond_to :json, only: [:create,:update,:destroy]
+#module Api
+  class OptionController < ApplicationController
+    respond_to :json, only: [:create,:update,:destroy]
 
-  def create
-    @option = Option.new(option_params)
-    @option.save
-  end
-
-  def update
-    @option = option_find
-    if !@option
-      render json: {error: "Error: No existe la opción con id #{id}"},
-             status: :unprocessable_entity
-    else
-      format.json { render json: @option.errors, status: :unprocessable_entity }
+    def create
+      @option = Option.new(option_params)
+      @option.save
     end
-    @option.update(option_params)
-#    redirect_to_maker_view
-  end
 
-  def destroy
-    @option=option_find
-    @option.destroy
+    def update
+      @option = option_find
+      if !@option
+        render json: {error: "Error: No existe la opción con id #{id}"},
+             status: :unprocessable_entity
+      else
+        format.json { render json: @option.errors, status: :unprocessable_entity }
+      end
+      @option.update(option_params)
+#     redirect_to_maker_view
+    end
+
+    def destroy
+      @option=option_find
+      @option.destroy
     #   redirect_to_maker_view
-  end
+    end
 
-  private
-  def option_find
-    @option = Option.find(params[:id])
-  end
+    private
+    def option_find
+      @option = Option.find(params[:id])
+    end
 
-  def option_params
-    params
+    def option_params
+      params
         .require(:chapter)
         .permit(
             :id,
@@ -38,6 +39,6 @@ class OptionController < ApplicationController
             :child_id,
             :option
         )
+    end
   end
-
-end
+#end
